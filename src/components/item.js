b2365cfccf6092bede7de45f51ad0be483176547
ayalaguttman <82307669+ayalaguttman@users.noreply.github.com>
2023-07-05 01:52:20 +0300

    update item & orders

--- src/components/item.js
+++ src/components/item.js
@@ -14,6 +14,11 @@ export default function Item() {
 
   const [name, setName] = useState("item");
   const [image, setImage] = useState("img");
+  const [color, setColor] = useState("color");
+  const [info, setInfo] = useState("info");
+  const [price, setPrice] = useState("price");
+
+
 
 
   useEffect(() => {
@@ -21,28 +26,81 @@ export default function Item() {
 
     const storedItem = localStorage.getItem('itemName');
     const storedImage = localStorage.getItem('img');
+    const storedColor = localStorage.getItem('color');
+    const storedInfo = localStorage.getItem('info');
+    const storedPrice = localStorage.getItem('price');
+
 
     if (storedItem) {
       setName(storedItem);
     } else {
-      setName('Item');
+      setName('No Item');
     }
 
-    
+
     if (storedImage) {
       setImage(storedImage);
     } else {
-      setImage('image');
+      setImage(' No image');
+    }
+
+    if (storedInfo) {
+      setInfo(storedInfo);
+    } else {
+      setInfo('No info');
+    }
+
+    if (storedPrice) {
+      setPrice(storedPrice);
+    } else {
+      setPrice('No price');
+    }
+
+    if (storedColor) {
+      setColor(storedColor);
+    } else {
+      setColor('No color');
     }
 
   }, []);
 
+  const order = () => {
+    // מחיקת טוקן
+    window.confirm("Order sent, your position in queue 2")
+      
+  }
+
   return (
-    <div className='container'>
-      <h1 className='text-center text-light'>{name}</h1>
-      <div>
-        <img src={"../images/" + image} width={100} alt={image}></img>
+    <div className="container">
+      <div className="row justify-content-center">
+
+      <div className="card bg-light bg-opacity-75 w-50 ">
+      <div className="row justify-content-center">
+        <img src={"../images/" + image}  className="card-img-top w-50 " alt={image}></img>
+        <div className="card-body">
+          <h5 className="card-title text-secondary">{name}</h5>
+          <p className="card-text">{info}</p>
+          <p className="card-text">{color}</p>
+          <p className="card-text text-success">{price}</p>
+          <select className="form-select mb-2" aria-label="Default select example">
+                    <option selected>select size</option>
+                    <option value="36">36</option>
+                    <option value="36">38</option>
+                    <option value="36">40</option>
+                    <option value="36">42</option>
+
+                </select>
+          <button  onClick={order} className="btn btn-primary">Order</button>
+
+        </div>
+        </div>
+
+        </div>
       </div>
-    </div>
+      </div>
+
+   
+
+
   )
 }
--- src/components/orders.js
+++ src/components/orders.js
@@ -12,35 +12,43 @@ import Login from './login.js';
 
 export default function Oreders() {
 
-  const [name, setName] = useState("item");
-  const [image, setImage] = useState("img");
+    const [name, setName] = useState("item");
+    const [image, setImage] = useState("img");
 
 
-  useEffect(() => {
-    // Check if the name exists in the local storage
+    useEffect(() => {
+        // Check if the name exists in the local storage
 
-    const storedItem = localStorage.getItem('itemName');
-    const storedImage = localStorage.getItem('img');
+        const storedItem = localStorage.getItem('itemName');
+        const storedImage = localStorage.getItem('img');
 
-    if (storedItem) {
-      setName(storedItem);
-    } else {
-      setName('Item');
-    }
+        if (storedItem) {
+            setName(storedItem);
+        } else {
+            setName('Item');
+        }
 
-    
-    if (storedImage) {
-      setImage(storedImage);
-    } else {
-      setImage('image');
-    }
 
-  }, []);
+        if (storedImage) {
+            setImage(storedImage);
+        } else {
+            setImage('image');
+        }
 
-  return (
-    <div className='container'>
-      <h1 className='text-center text-light'>Please select a branch near you</h1>
-      <select></select>
-    </div>
-  )
+    }, []);
+
+    return (
+        <div className='container'>
+            <div className='col-md-6 p-3 shadow mx-auto text-center'>
+
+                <h1 className='text-center text-light'>Please select a branch near you</h1>
+                <select class="form-select" aria-label="Default select example">
+                    <option selected>Petah - Tikva</option>
+                    <option value="Ramat - Gan">Ramat - Gan</option>
+                    <option value="Tel - Aviv">Tel - Aviv</option>
+                    <option value="TTTTTizi">TTTTTizi</option>
+                </select>
+            </div>
+        </div>
+    )
 }
